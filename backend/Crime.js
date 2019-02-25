3188d17dc5a72fc8e7ad0ed15b187d3910467a0a
jansedav <jansedav@oregonstate.edu>
2019-02-24 21:51:23 -0800

    Changed formatting for master.csv, made the API gather data totals for each states crime as well as specific states.

--- backend/Crime.js
+++ backend/Crime.js
@@ -5,8 +5,41 @@ const CrimeSchema = require('./CrimeModel');
 
 
 router.get('/', async (req, res) => {
-    const crime = await CrimeSchema.find();
+    const crime = await CrimeSchema.aggregate([{ $group: { _id: "$State", 
+    Violentcrime: { $sum: "$ViolentCrime" },
+    Totalmurderandmanslaughter: {$sum: "$MurderManslaughter"},
+    Totalrape: {$sum: "$Rape"},
+    TotalRobbery: {$sum: "$Robbery"},
+    TotalAssault: {$sum: "$Assault"},
+    TotalPropertyCrime: {$sum: "$PropertyCrime"},
+    TotalBurglary: {$sum: "$Burglary"},
+    TotalTheft: {$sum: "$Theft"},
+    TotalMotorTheft: {$sum: "$MotorTheft"},
+    TotalArson: {$sum: "$Arson"}
+    }}]) 
+
+    if(!crime) res.status(404)
+    .send('The given state did not return any data!')
     res.send(crime);
+});
+
+router.get('/:state', async(req, res) => {
+    const state = await CrimeSchema.aggregate([{ $group: { _id: req.params.state, 
+    Violentcrime: { $sum: "$ViolentCrime" },
+    Totalmurderandmanslaughter: {$sum: "$MurderManslaughter"},
+    Totalrape: {$sum: "$Rape"},
+    TotalRobbery: {$sum: "$Robbery"},
+    TotalAssault: {$sum: "$Assault"},
+    TotalPropertyCrime: {$sum: "$PropertyCrime"},
+    TotalBurglary: {$sum: "$Burglary"},
+    TotalTheft: {$sum: "$Theft"},
+    TotalMotorTheft: {$sum: "$MotorTheft"},
+    TotalArson: {$sum: "$Arson"}
+    }}]) 
+
+if(!state) res.status(404)
+.send('The given state did not return any data!')
+res.send(state);
 });
 
  module.exports = router;
