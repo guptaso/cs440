b6899845649fe0d9d19ca922e1bd00c956e249bc
jansedav <jansedav@oregonstate.edu>
2019-02-26 19:07:40 -0800

    Added the state/crime API call and fixed the call for each individual state

--- backend/Crime.js
+++ backend/Crime.js
@@ -5,7 +5,8 @@ const CrimeSchema = require('./CrimeModel');
 
 
 router.get('/', async (req, res) => {
-    const crime = await CrimeSchema.aggregate([{ $group: { _id: "$State", 
+    const crime = await CrimeSchema.aggregate([
+    { $group: { _id: "$State", 
     Violentcrime: { $sum: "$ViolentCrime" },
     Totalmurderandmanslaughter: {$sum: "$MurderManslaughter"},
     Totalrape: {$sum: "$Rape"},
@@ -24,22 +25,42 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:state', async(req, res) => {
-    const state = await CrimeSchema.aggregate([{ $group: { _id: req.params.state, 
-    Violentcrime: { $sum: "$ViolentCrime" },
-    Totalmurderandmanslaughter: {$sum: "$MurderManslaughter"},
-    Totalrape: {$sum: "$Rape"},
-    TotalRobbery: {$sum: "$Robbery"},
-    TotalAssault: {$sum: "$Assault"},
-    TotalPropertyCrime: {$sum: "$PropertyCrime"},
-    TotalBurglary: {$sum: "$Burglary"},
-    TotalTheft: {$sum: "$Theft"},
-    TotalMotorTheft: {$sum: "$MotorTheft"},
-    TotalArson: {$sum: "$Arson"}
-    }}]) 
+    const state = await CrimeSchema.aggregate([
+        { $match: { State: { $eq: req.params.state
+        }}},    
+        { $group: { _id: "$State", 
+        Violentcrime: { $sum: "$ViolentCrime" },
+        Totalmurderandmanslaughter: {$sum: "$MurderManslaughter"},
+        Totalrape: {$sum: "$Rape"},
+        TotalRobbery: {$sum: "$Robbery"},
+        TotalAssault: {$sum: "$Assault"},
+        TotalPropertyCrime: {$sum: "$PropertyCrime"},
+        TotalBurglary: {$sum: "$Burglary"},
+        TotalTheft: {$sum: "$Theft"},
+        TotalMotorTheft: {$sum: "$MotorTheft"},
+        TotalArson: {$sum: "$Arson"}
+        }}]) 
 
-if(!state) res.status(404)
-.send('The given state did not return any data!')
-res.send(state);
+    if(!state) res.status(404)
+    .send('The given state did not return any data!')
+    res.send(state);
+    });
+
+router.get('/:state/:crime', async(req, res) => {
+    console.log(req.params.crime);
+
+    const State = await CrimeSchema.aggregate([
+        { $match: { State: { $eq: req.params.state
+        }}},    
+        { $group: { _id: "$State", 
+        TotalCrime: { $sum: "$" + req.params.crime }
+        }}]) 
+
+    if(!State) res.status(404)
+    .send('The given state did not return any data!')
+    res.send(State);
 });
+
+
 
  module.exports = router;
